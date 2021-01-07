1a3733e150e86dbec7ce008e2fa493774d1e5c4b
ibrahimrammal <rammal.ibrahim.a@gmail.com>
2021-01-07 02:34:18 +0200

    db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //mongodb://localhost:27017/demo
 //process.env.MONGODB_URL
-mongoose.connect("mongodb://localhost:27017/demo", {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
