4ec521e0e9cee6eb7f83c0cb514400675207c3e0
root <root@vastab.vastlb.com>
2021-02-03 16:43:11 +0100

    nothing

---
+++ db/test.js
@@ -0,0 +1,17 @@
+// const todos =  [
+//     {
+//       id: 1,
+//       title: "lunch",
+//       description: "Go for lunc by 2pm"
+//     }
+// ];
+
+var todos = [{OrderID:10001,"CustomerID":"ALFKI","EmployeeID":1,"Freight":2.3,"ShipCity":"Berlin","Verified":false,"OrderDate":"1991-05-15T00:00:00","ShipName":"Simons bistro","ShipCountry":"Denmark","ShippedDate":"1996-07-16T00:00:00","ShipAddress":"Kirchgasse 6"}]
+
+//export default todos;
+
+
+//const todos = mongoose.model('EmptyTemplate', emptySchema)
+
+module.exports = todos
+
