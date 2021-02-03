fb02903de6bf351c92731987b4747a88961b5f43
ibrahimrammal <rammal.ibrahim.a@gmail.com>
2021-02-03 17:38:57 +0200

    FamilyExtract

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 //mongodb://localhost:27017/demo
 //process.env.MONGODB_URL
+//console.log(process.env.MONGODB_URL);
+
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
